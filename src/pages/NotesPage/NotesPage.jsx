3a347a270793d73dd3a8d13a8cddec035362f3cf
afdon <afdonovan@alumni.princeton.edu>
2024-09-23 22:54:42 -0700

    mouse events

--- src/pages/NotesPage/NotesPage.jsx
+++ src/pages/NotesPage/NotesPage.jsx
@@ -7,6 +7,24 @@ const NoteCard = ({ note }) => {
         document.addEventListener("mousemove", mouseMove);
     };
 
+    const mouseMove = (e) => {
+        //1 - Calculate move direction
+        let mouseMoveDir = {
+            x: mouseStartPos.x - e.clientX,
+            y: mouseStartPos.y - e.clientY,
+        };
+     
+        //2 - Update start position for next move.
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+     
+        //3 - Update card top and left position.
+        setPositon({
+            x: cardRef.current.offsetLeft - mouseMoveDir.x,
+            y: cardRef.current.offsetTop - mouseMoveDir.y,
+        });
+    };
+
     let position = JSON.parse(note.position);
     const colors = JSON.parse(note.colors);
     const body = JSON.parse(note.body);
