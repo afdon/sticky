2858a93ac5f6e5c68f332a17d07b7f246b0e0d32
afdon <afdonovan@alumni.princeton.edu>
2024-09-23 23:00:45 -0700

    wip notecard

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,11 +1,62 @@
+import { useEffect, useState } from "react";
+
 const NoteCard = ({ note }) => {
-  let mouseStartPos = { x: 0, y: 0 };
+    let mouseStartPos = { x: 0, y: 0 };
 
-  const cardRef = useRef(null);
+    const [position, setPosition] = useState(JSON.parse(note.position));
+    const body = bodyParser(note.body);
 
-  const [position, setPosition] = useState(JSON.parse(note.position));
+    useEffect(() => {
+        autoGrow(textAreaRef);
+        setZIndex(cardRef.current);
+    }, []);
 
-  const body = JSON.parse(note.body);
+    const mouseDown = (e) => {
+        if (e.target.className === "card-header") {
+            mouseStartPos.x = e.clientX;
+            mouseStartPos.y = e.clientY;
 
-  return <div>{body}</div>;
+            setZIndex(cardRef.current);
+
+            document.addEventListener("mousemove", mouseMove);
+            document.addEventListener("mouseup", mouseUp);
+            setSelectedNote(note);
+        }
+    };
+
+    const mouseMove = (e) => {
+        const mouseMoveDir = {
+            x: mouseStartPos.x - e.clientX,
+            y: mouseStartPos.y - e.clientY,
+        };
+
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+
+        const newPosition = setNewOffset(cardRef.current, mouseMoveDir);
+        setPosition(newPosition);
+    };
+
+    return (
+        <div
+
+        >
+            <div
+                onMouseDown={mouseDown}
+                className="card-header"
+                style={{
+                    backgroundColor: colors.colorHeader,
+                }}
+            >
+
+            </div>
+            <div className="card-body">
+                <textarea
+                    
+                ></textarea>
+            </div>
+        </div>
+    );
 };
+
+export default NoteCard;
--- src/pages/NotesPage/NotesPage.jsx
+++ src/pages/NotesPage/NotesPage.jsx
@@ -1,42 +1,21 @@
-const NoteCard = ({ note }) => {
+// import { fakeData as notes } from "../assets/fakeData.js";
+// import { db } from "../appwrite/databases";
+import NoteCard from "../components/NoteCard";
+import { useState, useEffect } from "react";
+import Controls from "../components/Controls";
+import { useContext } from "react";
+import { NotesContext } from "../context/NotesContext";
 
-    const mouseDown = (e) => {
-        mouseStartPos.x = e.clientX;
-        mouseStartPos.y = e.clientY;
-     
-        document.addEventListener("mousemove", mouseMove);
-    };
-
-    const mouseMove = (e) => {
-        //1 - Calculate move direction
-        let mouseMoveDir = {
-            x: mouseStartPos.x - e.clientX,
-            y: mouseStartPos.y - e.clientY,
-        };
-     
-        //2 - Update start position for next move.
-        mouseStartPos.x = e.clientX;
-        mouseStartPos.y = e.clientY;
-     
-        //3 - Update card top and left position.
-        setPositon({
-            x: cardRef.current.offsetLeft - mouseMoveDir.x,
-            y: cardRef.current.offsetTop - mouseMoveDir.y,
-        });
-    };
-
-    let position = JSON.parse(note.position);
-    const colors = JSON.parse(note.colors);
-    const body = JSON.parse(note.body);
- 
+const NotesPage = () => {
+    const { notes } = useContext(NotesContext);
     return (
-        <div
-            className="card"
-            style={{
-                backgroundColor: colors.colorBody,
-            }}
-        >
-            {body}
+        <div>
+            {notes.map((note) => (
+                <NoteCard note={note} key={note.$id} />
+            ))}
+            <Controls />
         </div>
     );
 };
+
+export default NotesPage;
