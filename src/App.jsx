67401b2c346181953c9e18a76cdfe501b8994104
afdon <afdonovan@alumni.princeton.edu>
2024-09-23 22:28:48 -0700

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-
-
+import NotesPage from "./pages/NotesPage";
+ 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <div id="app">
+            <NotesPage />
+        </div>
+    );
 }
-
-export default App
