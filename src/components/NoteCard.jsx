c6722bd98aa63818df8faa8eca2e29e57a356fa7
afdon <afdonovan@alumni.princeton.edu>
2024-09-23 22:50:23 -0700

    set position

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,4 +1,7 @@
 const NoteCard = ({ note }) => {
+    
+    const [position, setPosition] = useState(JSON.parse(note.position));
+
     const body = JSON.parse(note.body);
  
     return <div>{body}</div>;
