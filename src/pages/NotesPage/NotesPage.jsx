ad9e1ccb365bdd3480ae7a7bcada3997d23e980b
afdon <afdonovan@alumni.princeton.edu>
2024-09-23 22:54:00 -0700

    mousedown

--- src/pages/NotesPage/NotesPage.jsx
+++ src/pages/NotesPage/NotesPage.jsx
@@ -1,4 +1,12 @@
 const NoteCard = ({ note }) => {
+
+    const mouseDown = (e) => {
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+     
+        document.addEventListener("mousemove", mouseMove);
+    };
+
     let position = JSON.parse(note.position);
     const colors = JSON.parse(note.colors);
     const body = JSON.parse(note.body);
