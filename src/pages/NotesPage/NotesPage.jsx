d69d0365a75acfa506d842d8943c50a9822c8879
afdon <afdonovan@alumni.princeton.edu>
2024-09-23 22:33:58 -0700

    fix

--- src/pages/NotesPage/NotesPage.jsx
+++ src/pages/NotesPage/NotesPage.jsx
@@ -1,3 +1,12 @@
+import { fakeData as notes } from "../assets/fakeData.js";
+import NoteCard from "../components/NoteCard";
+ 
 const NotesPage = () => {
-    return <div>NotesPage</div>;
+    return (
+        <div>
+            {notes.map((note) => (
+                <NoteCard note={note} key={note.$id} />
+            ))}
+        </div>
+    );
 };
